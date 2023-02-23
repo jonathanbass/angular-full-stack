df0d4cfceb03f089ddc542a2fb17f8bd5dc683af
Jonathan Bass <jonathancharlesbass@gmail.com>
2023-02-23 16:13:38 +0000

    Added better number validation for runtime

--- client/src/app/components/add-movie/add-movie.component.ts
+++ client/src/app/components/add-movie/add-movie.component.ts
@@ -1,5 +1,5 @@
-import { Component, Pipe, PipeTransform } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MovieDataService } from 'src/app/services/movie.data.service';
 
 @Component({
@@ -11,25 +11,28 @@ import { MovieDataService } from 'src/app/services/movie.data.service';
 export class AddMovieComponent {
   form = new FormGroup({
     title: new FormControl('', [Validators.required]),
-    runtime: new FormControl('', [Validators.required, Validators.min(0), Validators.max(1000)])
+    runtime: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(1000)])
   });
-  
+
   constructor(private movieDataService: MovieDataService) {
 
   }
+
   get title() { return this.form?.get('title'); }
 
   get runtime() { return this.form?.get('runtime'); }
 
   createMovie(event: Event) {
     event.preventDefault();
-    if (this.form?.valid) {
-      const newMovie = {
-        title: this.form.get('title')?.getRawValue() || '',
-        runtime: (this.form.get('runtime')?.getRawValue() || 0) as number
-      };
-
-      this.movieDataService.create(newMovie);
+    if (this.form.invalid) {
+      return;
     }
+
+    const newMovie = {
+      title: this.form.get('title')?.value || undefined,
+      runtime: this.form.get('runtime')?.value || undefined
+    };
+
+    this.movieDataService.create(newMovie);
   }
 }
